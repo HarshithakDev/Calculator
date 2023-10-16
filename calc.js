5266dd976daa82a5efb9bdc38dedbe347524fe38
Harshitha K <harshitha@talentbridge.co.in>
2023-10-16 07:59:20 -0700

    added over writing of signs

--- calc.js
+++ calc.js
@@ -1,18 +1,25 @@
 let result=null;
 let input= " ";
+let histdata=[];
+let expression = " ";
+let result_data =" ";
+
 function updateDisplay(){
 	document.getElementById("inputext").value=input;
 
 }
 function appendCharacter(character) {
+	const lastChar = input.slice(-1);
 	if (character === '%' && input !== "") {
 		input = (parseFloat(input) / 100).toString();
-	} else {
+	} else if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
+		input = input.slice(0, -1) + character;
+	}
+	else {
 		input += character;
 	}
 	updateDisplay();
 }
-
 function clearDisplay(){
 	input="";
 	result=null;
@@ -36,13 +43,31 @@ function DeletelastCharacter() {
 function calculateResult() {
 	try {
 		result = eval(input);
+		result_data = result;
+		histdata.push({ "expression":expression,"result":result_data});
+		showHistory();
+		result_data="" ;
+		expression = "";
 		input = result.toString()
 		updateDisplay();
+		
 	} catch (error) {
 		input = "Error";
 		updateDisplay();
 		result = null;
 	}
 }
+function showHistory(){
+	let log = document.getElementById('hist');
+	let str="";
+	for( let K in histdata){
+		str+=histdata[K]["expression"]+"="+histdata[K]["result"]+"<br>";
+		log.innerHTML=str;
+		updateDisplay();
+	}
+	
+}
+
+
 
 
